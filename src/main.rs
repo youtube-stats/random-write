ee7028a2ce51ec2ceea09edd6870f5918715e6a6
Salvador Guzman <guzmansalv@gmail.com>
2019-05-28 16:22:15 -0700

    FINALLY WORKING - USE PROST VERSION 0.4.0

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,53 @@ extern crate actix;
 extern crate actix_protobuf;
 extern crate actix_web;
 extern crate bytes;
+extern crate env_logger;
 extern crate prost;
 #[macro_use]
 extern crate prost_derive;
 
 use actix_protobuf::*;
 use actix_web::*;
-use actix_web::web::{Data, resource, get, post};
+use actix_web::web::{Data, resource, get};
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc::channel;
 use std::thread;
 
 #[derive(Clone, PartialEq, Message)]
-pub struct MyObj {
-    #[prost(int32, tag = "1")]
-    pub number: i32,
-    #[prost(string, tag = "2")]
-    pub name: String,
+pub struct Subs {
+    #[prost(int32, repeated, tag = "1")]
+    pub id: Vec<i32>,
+    #[prost(int32, repeated, tag = "2")]
+    pub subs: Vec<i32>,
 }
 
-fn index(msg: ProtoBuf<MyObj>) -> HttpResponse {
+#[derive(Clone, PartialEq, Message)]
+pub struct Views {
+    #[prost(int32, repeated, tag = "1")]
+    pub id: Vec<i32>,
+    #[prost(int32, repeated, tag = "2")]
+    pub views: Vec<i32>,
+}
+
+#[derive(Clone, PartialEq, Message)]
+pub struct Videos {
+    #[prost(int32, repeated, tag = "1")]
+    pub id: Vec<i32>,
+    #[prost(int32, repeated, tag = "2")]
+    pub videos: Vec<i32>,
+}
+
+#[derive(Clone, PartialEq, Message)]
+pub struct Metrics {
+    #[prost(message, tag = "1")]
+    pub subs: Option<Subs>,
+    #[prost(message, tag = "2")]
+    pub views: Option<Views>,
+    #[prost(message, tag = "3")]
+    pub videos: Option<Videos>
+}
+
+fn index(msg: ProtoBuf<Metrics>) -> HttpResponse {
     println!("model: {:?}", msg);
     HttpResponse::Ok().finish()
 }
@@ -70,6 +97,9 @@ fn put_videos(state: Data<Receivers>, req: HttpRequest) -> HttpResponse {
 
 fn main() {
     println!("Hello, world!");
+    ::std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    let sys = actix::System::new("prost-example");
 
     let (mut subs_tx, mut subs_rx): (Sender<(i32,i32)>, Receiver<(i32,i32)>) = channel();
     let (mut views_tx, mut views_rx): (Sender<(i32,i32)>, Receiver<(i32,i32)>) = channel();
@@ -105,19 +135,10 @@ fn main() {
             views: views_tx.clone(),
             videos: videos_tx.clone()
         })
+        .wrap(middleware::Logger::default())
         .service(
             resource("/put")
-                .route(post().to(put)))
-        .service(
-            resource("/subs")
-                .route(get().to(put_subs)))
-        .service(
-            resource("/views")
-                .route(get().to(put_views)))
-        .service(
-            resource("/videos")
-                .route(get().to(put_videos)))
-
+                .route(get().to(put)))
     ).bind("0.0.0.0:8080")
     .expect("Can not bind to port 8080")
     .run()
