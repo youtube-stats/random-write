ecc68da15522e33aefccb21beb23662529e676c4
Salvador Guzman <guzmansalv@gmail.com>
2019-06-10 11:09:32 -0700

    change protobuf message schema

--- src/message.rs
+++ src/message.rs
@@ -10,23 +10,24 @@
 
 
 use std::io::Write;
+use std::borrow::Cow;
 use quick_protobuf::{MessageRead, MessageWrite, BytesReader, Writer, Result};
 use quick_protobuf::sizeofs::*;
 use super::*;
 
 #[derive(Debug, Default, PartialEq, Clone)]
-pub struct SubMessage {
+pub struct ChannelMessage<'a> {
     pub ids: Vec<i32>,
-    pub subs: Vec<i32>,
+    pub serial: Vec<Cow<'a, str>>,
 }
 
-impl<'a> MessageRead<'a> for SubMessage {
+impl<'a> MessageRead<'a> for ChannelMessage<'a> {
     fn from_reader(r: &mut BytesReader, bytes: &'a [u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
             match r.next_tag(bytes) {
                 Ok(10) => msg.ids = r.read_packed(bytes, |r, bytes| Ok(r.read_int32(bytes)?))?,
-                Ok(18) => msg.subs = r.read_packed(bytes, |r, bytes| Ok(r.read_int32(bytes)?))?,
+                Ok(18) => msg.serial.push(r.read_string(bytes).map(Cow::Borrowed)?),
                 Ok(t) => { r.read_unknown(bytes, t)?; }
                 Err(e) => return Err(e),
             }
@@ -35,47 +36,16 @@ impl<'a> MessageRead<'a> for SubMessage {
     }
 }
 
-impl MessageWrite for SubMessage {
+impl<'a> MessageWrite for ChannelMessage<'a> {
     fn get_size(&self) -> usize {
         0
         + if self.ids.is_empty() { 0 } else { 1 + sizeof_len(self.ids.iter().map(|s| sizeof_varint(*(s) as u64)).sum::<usize>()) }
-        + if self.subs.is_empty() { 0 } else { 1 + sizeof_len(self.subs.iter().map(|s| sizeof_varint(*(s) as u64)).sum::<usize>()) }
+        + self.serial.iter().map(|s| 1 + sizeof_len((s).len())).sum::<usize>()
     }
 
     fn write_message<W: Write>(&self, w: &mut Writer<W>) -> Result<()> {
         w.write_packed_with_tag(10, &self.ids, |w, m| w.write_int32(*m), &|m| sizeof_varint(*(m) as u64))?;
-        w.write_packed_with_tag(18, &self.subs, |w, m| w.write_int32(*m), &|m| sizeof_varint(*(m) as u64))?;
-        Ok(())
-    }
-}
-
-#[derive(Debug, Default, PartialEq, Clone)]
-pub struct Ack {
-    pub ok: bool,
-}
-
-impl<'a> MessageRead<'a> for Ack {
-    fn from_reader(r: &mut BytesReader, bytes: &'a [u8]) -> Result<Self> {
-        let mut msg = Self::default();
-        while !r.is_eof() {
-            match r.next_tag(bytes) {
-                Ok(8) => msg.ok = r.read_bool(bytes)?,
-                Ok(t) => { r.read_unknown(bytes, t)?; }
-                Err(e) => return Err(e),
-            }
-        }
-        Ok(msg)
-    }
-}
-
-impl MessageWrite for Ack {
-    fn get_size(&self) -> usize {
-        0
-        + if self.ok == false { 0 } else { 1 + sizeof_varint(*(&self.ok) as u64) }
-    }
-
-    fn write_message<W: Write>(&self, w: &mut Writer<W>) -> Result<()> {
-        if self.ok != false { w.write_with_tag(8, |w| w.write_bool(*&self.ok))?; }
+        for s in &self.serial { w.write_with_tag(18, |w| w.write_string(&**s))?; }
         Ok(())
     }
 }
