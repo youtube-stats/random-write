837df5350b843661502569bd4f19e5aecaa32cd5
Salvador Guzman <guzmansalv@gmail.com>
2019-06-13 15:25:56 -0700

    start from scratch

--- src/message.rs
+++ src/message.rs
@@ -16,12 +16,12 @@ use quick_protobuf::sizeofs::*;
 use super::*;
 
 #[derive(Debug, Default, PartialEq, Clone)]
-pub struct ChannelMessage<'a> {
+pub struct ChannelRowMessage<'a> {
     pub ids: Vec<i32>,
     pub serials: Vec<Cow<'a, str>>,
 }
 
-impl<'a> MessageRead<'a> for ChannelMessage<'a> {
+impl<'a> MessageRead<'a> for ChannelRowMessage<'a> {
     fn from_reader(r: &mut BytesReader, bytes: &'a [u8]) -> Result<Self> {
         let mut msg = Self::default();
         while !r.is_eof() {
@@ -36,7 +36,7 @@ impl<'a> MessageRead<'a> for ChannelMessage<'a> {
     }
 }
 
-impl<'a> MessageWrite for ChannelMessage<'a> {
+impl<'a> MessageWrite for ChannelRowMessage<'a> {
     fn get_size(&self) -> usize {
         0
         + if self.ids.is_empty() { 0 } else { 1 + sizeof_len(self.ids.iter().map(|s| sizeof_varint(*(s) as u64)).sum::<usize>()) }
@@ -81,37 +81,6 @@ impl MessageWrite for SubMessage {
     fn write_message<W: Write>(&self, w: &mut Writer<W>) -> Result<()> {
         w.write_packed_with_tag(10, &self.ids, |w, m| w.write_int32(*m), &|m| sizeof_varint(*(m) as u64))?;
         w.write_packed_with_tag(18, &self.subs, |w, m| w.write_int32(*m), &|m| sizeof_varint(*(m) as u64))?;
-        Ok(())
-    }
-}
-
-#[derive(Debug, Default, PartialEq, Clone)]
-pub struct Ack {
-    pub ok: bool,
-}
-
-impl<'a> MessageRead<'a> for Ack {
-    fn from_reader(r: &mut BytesReader, bytes: &'a [u8]) -> Result<Self> {
-        let mut msg = Self::default();
-        while !r.is_eof() {
-            match r.next_tag(bytes) {
-                Ok(8) => msg.ok = r.read_bool(bytes)?,
-                Ok(t) => { r.read_unknown(bytes, t)?; }
-                Err(e) => return Err(e),
-            }
-        }
-        Ok(msg)
-    }
-}
-
-impl MessageWrite for Ack {
-    fn get_size(&self) -> usize {
-        0
-        + if self.ok == false { 0 } else { 1 + sizeof_varint(*(&self.ok) as u64) }
-    }
-
-    fn write_message<W: Write>(&self, w: &mut Writer<W>) -> Result<()> {
-        if self.ok != false { w.write_with_tag(8, |w| w.write_bool(*&self.ok))?; }
         Ok(())
     }
 }
--- src/statics.rs
+++
@@ -1,3 +0,0 @@
-pub const KEY_URL: &'static str = "http://127.0.0.1:8080/get";
-pub const WRITE_URL: &'static str = "http://127.0.0.1:8081/post";
-pub const QUERY_URL: &'static str = "http://127.0.0.1:8082";
